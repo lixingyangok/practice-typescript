c8c0ac4abbbeec5fcaf18123855cf22df0295ea4
lixingyang <176840078@qq.com>
2019-10-29 21:17:29 +0800

    泛型

---
+++ js/zhinengshe/03-泛型.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getData(params) {
+    // return `${params} + ${new Date().toLocaleString()}`;
+    console.log(params);
+    return params;
+}
+getData(18);
+getData('tom');
---
+++ zhinengshe/03-泛型.ts
@@ -0,0 +1,10 @@
+import { type } from "os";
+
+function getData<myType>( params:myType ):myType {
+    // return `${params} + ${new Date().toLocaleString()}`;
+    console.log( params );
+    return params;
+}
+
+getData<number>(18);
+getData<string>('tom');
