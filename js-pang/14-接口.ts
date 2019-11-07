a771350ac707db37f9b88f1a9d029d1d12595c6e
lixingyang <176840078@qq.com>
2019-11-07 21:31:44 +0800

    ts接口

--- js-pang/14-接口.ts
+++ js-pang/14-接口.ts
@@ -1,25 +1,34 @@
 interface Human{ //定义一个对象接口
-    name:string;
-    sex:string;
-    age?:number; //添加“?”表示可选属性
+	name:string;
+	sex:string;
+	age?:number; //添加“?”表示可选属性
 }
 
+// ▼用接口约束对象
 let boyTom:Human = {
-    name:'Tom',
-    sex: '男', //如果属性缺少，会报错
-    // age: 18, //如果属性缺少，不报错
+	name:'Tom',
+	sex: '男', //如果属性缺少，会报错
+	// age: 18, //如果属性缺少，不报错
 };
 
-console.log( boyTom );
+// ▼用接口约束方法的参数
+function toLogHuman( thisOne:Human ) {
+	console.log( thisOne.name, thisOne.sex );
+}
+toLogHuman( boyTom );
+toLogHuman( {name:'Sara', sex: '女'} );
+
+
 
 interface MathFn{
-    ( n01:number, n02:number ):number;
+	( n01:number, n02:number ):number;
 }
+// ▼用接口约束一个方法的形态
 let getSumFn:MathFn = function( n01:number, n02:number ):number{
-    return n01 + n02;
+	return n01 + n02;
 }
 let getProduct:MathFn = function( n01:number, n02:number ):number{
-    return n01 * n02;
+	return n01 * n02;
 }
 console.log( getSumFn(3,3) );
 console.log( getProduct(3,3) );
